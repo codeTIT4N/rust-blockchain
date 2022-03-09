ed462d44dbea96f9d49612aee220a7c30d0001f9
codeTIT4N <lokeshkumar636@yahoo.com>
2022-03-10 00:59:37 +0530

    blockchain and block verification

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Basic blockchain implementation in rust with features like mining, transactions,
 
 Reference: Using tutorials in this youtube playlist: https://youtube.com/playlist?list=PLwnSaD6BDfXL0RiKT_5nOIdxTxZWpPtAv
 
-Mining Process:
+### Mining Process:
 
 Step 1: Generate a new nonce.
 
@@ -17,6 +17,20 @@ change nonce and go through step 1 till 4 till the check passes.
 Step 5: Add block to the blockchain.
 
 Step 6: Submit to peers (Not doing for now).
+
+### Verification process:
+
+We will verify 4 things now:
+
+1. Actual index == stored index value (note that Bitcoin blocks don't store their index).
+
+2. Block's hash fits stored difficulty value (we'll just trust the difficulty for now) (insecure).
+
+3. Time is always increasing (In Real Life network latency/sync demands leniency here).
+
+4. Actual previous block's hash == stored prev_block_hash value (except the genesis block).
+
+Note: Bitcoin protocol describes 19 steps for verification.
 
 ## To run the project: 
 Step 1: Make sure you have rust and cargo installed in your system.
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,48 @@
 use blockchainlib::*;
 
 fn main() {
+    let difficulty = 0x0000ffffffffffffffffffffffffffff;
     let mut block = Block::new(
         0,
         now(),
         vec![0; 32],
         0,
         "Genesis block!".to_owned(),
-        0x0000fffffffffffffffffffffffffff,
+        difficulty,
     );
-    block.hash = block.hash();
-
-    println!("{:?}", block);
-    block.mine();
-    println!("{:?}", block);
-
     //So in case of bitcoin they have set there difficulty in such a way that it takes 10 mins on
     //everyone's machine and miners try to find the nonce for the reward
+    block.mine();
+    println!("Mined Genesis block {:?}", &block);
+
+    let mut last_hash = block.hash.clone();
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+
+    println!("Verify: {}", &blockchain.verify());
+
+    for i in 1..10 {
+        //1 and including 10
+        let mut block = Block::new(
+            i,
+            now(),
+            last_hash, //previous block hash
+            0,
+            "Another block!".to_owned(),
+            difficulty,
+        );
+        //mine another block
+        block.mine();
+        println!("Mined block {:?}", &block);
+        last_hash = block.hash.clone(); //update last hash
+        blockchain.blocks.push(block);
+        //verify after mining
+        println!("Verify: {}", &blockchain.verify());
+    }
+
+    println!("To check if verification fails: ===================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    //Another : To check if verify is working, this return verify false:
+    blockchain.blocks[3].prev_block_hash[18] = 7;
+    println!("Verify: {}", &blockchain.verify());
 }
